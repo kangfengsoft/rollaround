d166b06b261355723d4ad08993e03ea0ac4ac3f1
flkflkflk02 <fangliangkang@163.com>
2013-08-25 22:36:50 +0800

    refs #6 enable/disable timed task

--- protected/admin/views/site/index.php
+++ protected/admin/views/site/index.php
@@ -1,5 +1,12 @@
 <?php
 /* @var $this SiteController */
-echo "hello world!";
 ?>
-		
+<h1>enable/disable timed task</h1>
+<br/>
+<?php
+if($enable){
+?>
+<a href="?r=admin/enableTimedTask&enable=false" class="btn btn-large btn-primary">disable</a>
+<?php }else{ ?>
+<a href="?r=admin/enableTimedTask&enable=true" class="btn btn-large btn-primary">true</a>
+<?php } ?>
