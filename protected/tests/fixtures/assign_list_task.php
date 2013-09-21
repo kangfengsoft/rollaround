20d617583f1e17ed78903650e08a606d53075326
flkflkflk02 <fangliangkang@163.com>
2013-09-21 17:59:39 +0800

    refs #29 add assign_list_task and list_task in tests/fixtures

---
+++ protected/tests/fixtures/assign_list_task.php
@@ -0,0 +1,19 @@
+<?php
+return array (
+		'1' => array (
+				'id' => 1,
+				'taobao_user_id' => '3600303259',
+				'num_iid' => '20000001',
+				'day' => 0,
+				'hour' => 0,
+				'exclude' => 0
+		),
+		'2' => array (
+				'id' => 2,
+				'taobao_user_id' => '3600303259',
+				'num_iid' => '20000002',
+				'day' => 1,
+				'hour' => 1,
+				'exclude' => 0
+		) 
+);
---
+++ protected/tests/fixtures/list_task.php
@@ -0,0 +1,25 @@
+<?php
+// $hour = 0;
+// $min = 0;
+// $second = 0;
+// $taskMonth = 1;
+// $taskDay = 0;
+// $taskYear = 2010;
+// $listTime = date ( 'Y-m-d H:i:s', mktime ( $hour, $min, $second, $taskMonth, $taskDay, $taskYear ) );
+
+//the day of the list_time is 2;
+$listTime = "2013-09-17 00:01:01";
+return array (
+		'1' => array (
+				'id' => 1,
+				'taobao_user_id' => '3600303259',
+				'num_iid' => '10000001',
+				'list_time' => $listTime
+		),
+		'2' => array (
+				'id' => 2,
+				'taobao_user_id' => '3600303259',
+				'num_iid' => '10000002',
+				'list_time' => $listTime
+		) 
+);
